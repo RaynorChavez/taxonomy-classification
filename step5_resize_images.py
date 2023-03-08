e5eb653b12ed1d6b78e41a5906eb9310e0af2058
Raynor Chavez <raynorkirksonchavez@gmail.com>
2023-03-09 00:18:13 +0800

    multithread step5

--- step5_resize_images.py
+++ step5_resize_images.py
@@ -1,22 +1,37 @@
 import os
 from PIL import Image
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 dataset_subset = "subset1k"
 
-def resize_image(image_path, new_size, output_folder):
+def resize_image(image_path, new_size):
     with Image.open(image_path) as image:
         image = image.resize(new_size)
-        output_path = os.path.join(output_folder, os.path.basename(image_path))
-        image.save(output_path)
+        return image
 
+
+def write_image(image, output_folder, filename, image_num):
+    output_path = os.path.join(output_folder, filename)
+    image.save(output_path)
+    if image_num % 500 == 0:
+        print(f"Processed {image_num} images...")
 
 def process_images_in_folder(folder_path, output_folder, new_size):
-    with ThreadPoolExecutor() as executor:
-        for filename in os.listdir(folder_path):
-            image_path = os.path.join(folder_path, filename)
-            executor.submit(resize_image, image_path, new_size, output_folder)
+    resize_pool = ThreadPoolExecutor()
+    write_pool = ThreadPoolExecutor()
+    futures = []
 
+    for i, filename in enumerate(os.listdir(folder_path)):
+        image_path = os.path.join(folder_path, filename)
+        future = resize_pool.submit(resize_image, image_path, new_size)
+        futures.append((future, filename, i + 1))
+
+    for future, filename, image_num in futures:
+        resized_image = future.result()
+        write_pool.submit(write_image, resized_image, output_folder, filename, image_num)
+
+    resize_pool.shutdown()
+    write_pool.shutdown()
 
 if __name__ == '__main__':
     folder_path = f'./data/{dataset_subset}_taxo_data_images'
@@ -24,3 +39,4 @@ if __name__ == '__main__':
     new_size = (225, 225)
     os.makedirs(output_folder, exist_ok=True)
     process_images_in_folder(folder_path, output_folder, new_size)
+
