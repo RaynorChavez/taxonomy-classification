8e5243a81598342b0f57126b0825d949a897516b
Raynor Chavez <raynorkirksonchavez@gmail.com>
2023-03-09 09:30:14 +0800

    added checking of existing resized image step5

--- step5_resize_images.py
+++ step5_resize_images.py
@@ -49,12 +49,11 @@ if __name__ == '__main__':
 """
 
 import os
-import pathlib
 import tensorflow as tf
 
 # Set the path to the directory containing the images
 dataset_subset = "subset1k"
-image_dir = pathlib.Path(f'./data/{dataset_subset}_taxo_data_images')
+image_dir = f'./data/{dataset_subset}_taxo_data_images'
 
 # Set the desired image size
 img_size = (224, 224)
@@ -74,23 +73,39 @@ def preprocess_image(file_path):
     return img
 
 # Get a list of all image files in the directory
-files = list(image_dir.glob('*.[jJ][pP][gG]')) + list(image_dir.glob('*.[jJ][pP][eE][gG]')) + list(image_dir.glob('*.[pP][nN][gG]'))
-
-# Create a dataset from the list of file paths
-dataset = tf.data.Dataset.from_tensor_slices([str(file) for file in files])
-
-# Use multiple threads to preprocess the images in parallel
-dataset = dataset.map(preprocess_image, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+files = [os.path.join(image_dir, f) for f in os.listdir(image_dir)
+         if f.endswith('.jpg') or f.endswith('.jpeg') or f.endswith('.png')]
 
 # Set the path to the directory to save the resized images
-save_dir = pathlib.Path(f'./data/taxo_data_images_{dataset_subset}_resized')
-save_dir.mkdir(parents=True, exist_ok=True)
+save_dir = f'./data/taxo_data_images_{dataset_subset}_resized'
 
-# Save the resized images
-for file_path, img in zip(files, dataset):
-    img_name = file_path.stem + '.jpg'
-    img_path = save_dir / img_name
-    img = tf.image.encode_jpeg(tf.cast(img * 255.0, tf.uint8))
-    tf.io.write_file(str(img_path), img)
+# Check if save_dir exists, create it if it doesn't
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
 
-    print(f"Resized {file_path} to {img_size} and saved to {img_path}")
+# Loop through the list of image files
+for i, file_path in enumerate(files):
+    # Get the image name and path for the resized image
+    img_name = os.path.splitext(os.path.basename(file_path))[0] + '.jpg'
+    img_path = os.path.join(save_dir, img_name)
+
+    # If the resized image already exists, skip this file
+    if os.path.exists(img_path):
+        print(f"Resized {file_path} already exists in {save_dir}")
+        continue
+
+    # Load and preprocess the image
+    try:
+        img = preprocess_image(file_path)
+    except Exception as e:
+        print(f"Error while processing {file_path}: {str(e)}")
+        continue
+
+    # Save the resized image
+    try:
+        img = tf.image.encode_jpeg(tf.cast(img * 255.0, tf.uint8))
+        tf.io.write_file(img_path, img)
+        print(f"Resized {file_path} to {img_size} and saved to {img_path}")
+    except Exception as e:
+        print(f"Error while saving {img_path}: {str(e)}")
+        continue
