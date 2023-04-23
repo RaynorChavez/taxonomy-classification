64fbdb8f98bacc208c26925c7c6eff3a060a6648
kgreyy <57283823+kgreyy@users.noreply.github.com>
2023-04-23 18:17:25 +0800

    Edit non-species scrape

--- step1e_filter_non_species_working.py
+++ step1e_filter_non_species_working.py
@@ -7,7 +7,7 @@ import multiprocessing as mp
 from pathlib import Path
 import json
 
-dataset_subset = "nonspecies_mono"
+dataset_subset = "nonspecies"
 # -1 for all
 desired_entries_count = 1000
 
@@ -18,10 +18,13 @@ P_IMAGE = "P18"
 Q_POLITICIAN = "Q82955"
 
 # Q_TAXON = "Q16521" # Q_MONOTYPIC_TAXON = "Q310890"
-Q_TAXON = "Q310890"
+Q_TAXON = "Q16521"
+Q_CLADE = "Q713623"
+Q_MONOTYPIC_TAXON = "Q310890"
 Q_SPECIES = "Q7432"
+all_taxon = ["Q16521"] + ["Q210958","Q310890","Q310891","Q667083","Q713623","Q763978","Q855769","Q857968","Q931051","Q962530","Q1783100","Q1849414","Q2612572","Q3750886","Q8073782","Q19862317","Q21014462","Q23038290","Q30456678","Q58051350","Q59772388","Q59779138","Q60458657","Q87756246","Q97312312","Q98961713","Q104795308","Q106686983","Q107303655","Q110533142","Q111611096","Q113298331","Q4886","Q42621"]
 
-def is_instance_of_taxon(item: WikidataItem, truthy: bool = True) -> bool:
+def is_instance_of_taxon_or_subclass(item: WikidataItem, truthy: bool = True) -> bool:
     """Return True if the Wikidata Item is an instance of taxon."""
     if truthy:
         claim_group = item.get_truthy_claim_group(P_INSTANCE_OF)
@@ -33,7 +36,7 @@ def is_instance_of_taxon(item: WikidataItem, truthy: bool = True) -> bool:
         for claim in claim_group
         if claim.mainsnak.snaktype == "value"
     ]
-    return Q_TAXON in taxon_qids
+    return not set(taxon_qids).isdisjoint(all_taxon)
 
 def is_rank_species(item: WikidataItem, truthy: bool = True) -> bool:
     """Return True if the Wikidata Item is of rank species."""
@@ -77,14 +80,14 @@ def has_occupation_politician(item: WikidataItem, truthy: bool = True) -> bool:
 Path('./data/').mkdir(parents=True, exist_ok=True)
 
 # create an instance of WikidataJsonDump
-wjd_dump_path = "./data/wikidata-20220103-all.json.gz"
+wjd_dump_path = "./data/wikidata-20230213-all.json.bz2"
 wjd = WikidataJsonDump(wjd_dump_path)
 
 # create an iterable of WikidataItem representing politicians
 processed = 0
 t1 = time.time()
 
-with open(f"./data/{dataset_subset}_filtered_taxons.json", "a") as output:
+with open(f"./data/{dataset_subset}_filtered_taxons_final.json", "a") as output:
     output.truncate(0)
     output.write("[")
     first_object = True  # flag to keep track of first object
@@ -92,7 +95,7 @@ with open(f"./data/{dataset_subset}_filtered_taxons.json", "a") as output:
 
         if entity_dict["type"] == "item":
             entity = WikidataItem(entity_dict)
-            if is_instance_of_taxon(entity):
+            if is_instance_of_taxon_or_subclass(entity):
                 if not is_rank_species(entity):
                     if not first_object:
                         # add a comma before each object after the first one
